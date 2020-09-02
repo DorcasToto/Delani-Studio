062631b45c4aa2f1d09f79088044b4e64a6520f9
Moringa School <dorcasmosonik@gmail.com>
2020-09-02 14:13:08 +0300

    portfolio on hoover

--- js/script.js
+++ js/script.js
@@ -1,62 +1,60 @@
 
-$(document).ready(function(){
-    $(".design").click(function(){
+$(document).ready(function () {
+    $(".design").click(function () {
         $("#designImg").toggle();
         $("#designContent").toggle();
     });
-    $(".development").click(function(){
+    $(".development").click(function () {
         $("#developmentImg").toggle();
         $("#developmentContent").toggle();
     });
-    $(".productManagement").click(function(){
+    $(".productManagement").click(function () {
         $("#managementImg").toggle();
         $("#managementContent").toggle();
     });
-    $("#img1").mouseover(function() {
+    $("#img1").mouseover(function () {
         $("#project1").show();
-    }).mouseout(function() {
+    }).mouseout(function () {
         $("#project1").hide();
     });
 
-    $("#img2").mouseover(function() {
+    $("#img2").mouseover(function () {
         $("#project2").show();
-    }).mouseout(function() {
+    }).mouseout(function () {
         $("#project2").hide();
     });
-    $("#img3").mouseover(function() {
+    $("#img3").mouseover(function () {
         $("#project3").show();
-    }).mouseout(function() {
+    }).mouseout(function () {
         $("#project3").hide();
     });
-    $("#img4").mouseover(function() {
+    $("#img4").mouseover(function () {
         $("#project4").show();
-    }).mouseout(function() {
+    }).mouseout(function () {
         $("#project4").hide();
     });
-    $("#img5").mouseover(function() {
+    $("#img5").mouseover(function () {
         $("#project5").show();
-    }).mouseout(function() {
+    }).mouseout(function () {
         $("#project5").hide();
     });
-    $("#img6").mouseover(function() {
+    $("#img6").mouseover(function () {
         $("#project6").show();
-    }).mouseout(function() {
+    }).mouseout(function () {
         $("#project6").hide();
     });
-    $("#img7").mouseover(function() {
+    $("#img7").mouseover(function () {
         $("#project7").show();
-    }).mouseout(function() {
+    }).mouseout(function () {
         $("#project7").hide();
     });
-    $("#img8").mouseover(function() {
+    $("#img8").mouseover(function () {
         $("#project8").show();
-    }).mouseout(function() {
+    }).mouseout(function () {
         $("#project8").hide();
     });
 });
 
 function main() {
-   alert("Thank you for reaching out to us..getting back to you")
-   
-
+    alert("Thank you for reaching out to us..getting back to you");
 }
